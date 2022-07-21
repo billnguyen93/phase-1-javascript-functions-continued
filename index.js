1396fb969ea32516ecc0a28a0c8e18bb38e1eb5a
Bill Nguyen <billfl93@gmail.com>
2022-07-21 02:32:22 -0400

    commit tree 4d836c7ea7b330ded4d6b89fe09695894dadf752

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(star = '*') {
+    return function (adjective = "emphatic") {
+        return `You are ${star}${adjective}${star}!`;
+    };
+};
+
+
